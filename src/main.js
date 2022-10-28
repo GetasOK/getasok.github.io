8ae66d51f6bbccd4d3ca5f10b1e21d78c66336cc
GetasOK <getas5@yandex.ru>
2022-10-28 16:26:19 +0300

    chore: change fonts loading

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import vuetify from "./plugins/vuetify";
-import { loadFonts } from "./plugins/webfontloader";
 
-loadFonts();
+//Fonts
+import "@fontsource/roboto";
+import "@fontsource/roboto/100.css";
+import "@fontsource/roboto/300.css";
 
 createApp(App).use(router).use(vuetify).mount("#app");
